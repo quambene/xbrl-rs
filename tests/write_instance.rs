1d4595a6b893a0aea1958a3b8090a4b43b56a503
quambene <contact.quambene@gmail.com>
2026-02-15 23:40:12 +0100

    Test write instance

--- tests/write_instance.rs
+++ tests/write_instance.rs
@@ -1,22 +1,26 @@
-use quick_xml::{Reader, Writer};
-use std::{fs::File, io::BufReader, path::Path};
-use xbrl_rs::XbrlInstance;
+use roxmltree::Document;
+use std::{path::PathBuf, str::FromStr};
+use xbrl_rs::{TaxonomySet, XmlWriter};
 
-const INSTANCE_BASE: &str = "test_data/instances/ebilanz";
-
-fn parse_instance(path: &Path) -> XbrlInstance {
-    let file = File::open(path).expect("failed to open instance file");
-    let mut reader = Reader::from_reader(BufReader::new(file));
-
-    XbrlInstance::from_xml(&mut reader).expect("failed to parse instance")
-}
+const TAXONOMY_ENTRY_POINT: &str = "test_data/taxonomies";
 
 #[test]
-fn write_instance_v64_balance_sheet_restaurateur() {
-    let path = Path::new(INSTANCE_BASE).join("v6.4/HandelsbilanzGastronom_PersG.xml");
-    let instance = parse_instance(&path);
+fn write_empty_instance() {
+    let entry_point = PathBuf::from_str(TAXONOMY_ENTRY_POINT).unwrap();
+    let gcd = "http://www.xbrl.de/taxonomies/de-gcd-2020-04-01/de-gcd-2020-04-01-shell.xsd";
+    let gaap = "http://www.xbrl.de/taxonomies/de-gaap-ci-2020-04-01/de-gaap-ci-2020-04-01-shell-fiscal.xsd";
+    let taxonomy =
+        TaxonomySet::discover(vec![gcd.to_owned(), gaap.to_owned()], entry_point).unwrap();
+    let instance = taxonomy.create_instance();
 
-    let mut writer: Writer<Vec<u8>> = Writer::new_with_indent(Vec::new(), b' ', 2);
+    let mut writer: XmlWriter<Vec<u8>> = XmlWriter::new(Vec::new());
 
     instance.to_xml(&mut writer).unwrap();
+
+    let xml = String::from_utf8(writer.into_inner()).unwrap();
+
+    // Test parsing the generated XML
+    let doc = Document::parse(&xml);
+
+    assert!(doc.is_ok());
 }
